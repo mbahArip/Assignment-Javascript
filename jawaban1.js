d63e63d5d64f875c0e33b3d8bfabccc8035599ee
mbaharip <main@mbaharip.me>
2022-03-09 13:49:19 +0700

    initial Commit

---
+++ jawaban1.js
@@ -0,0 +1,49 @@
+var a = 5;
+let b = 'kampus merdeka';
+const nama = 'Budi';
+let terdaftar = false;
+let lengkap_arr = [a, b, nama, terdaftar];
+
+/* Soal B
+ * Ubah kode sehingga dapat menampilkan baris 22 dalam console.
+ */
+terdaftar = true;
+
+function perkenalan() {
+	let asal = 'Indonesia';
+	return console.log('perkenalan nama saya ' + nama + ' nomor urut ' + a + ' sekarang sedang mengikuti ' + b + ' berasal dari ' + asal);
+}
+
+if (terdaftar === true) {
+  // Baris 22
+	console.log(nama + ' terdaftar sebagai kegiatan kampus merdeka');
+}
+
+/* Soal A
+ * Ambil index ke 2 dalam array di baris 5, dan tampilkan dalam console
+ */
+console.log('Array = ' + lengkap_arr[2]);
+
+a = b;
+// Error karena const tidak dapat diubah
+// nama = b;
+
+// Error karena asal berada di scope function perkenalan
+// console.log('asal diakses = ' + asal);
+console.log('a adalah = ' + a);
+console.log('b adalah = ' + b);
+
+/* Soal C
+ * Lakukan pemanggilan untuk function perkenalan
+ */
+perkenalan();
+
+/* Soal D
+ * Ubah kode sehinggal menampilkan output:
+ *
+ * Budi terdaftar sebagai kegiatan kampus merdeka
+ * Array = Budi
+ * a adalah = kampus merdeka
+ * b adalah = kampus merdeka
+ * perkenalan nama saya Budi nomor urut 5 sekarang sedang mengikuti kampus merdeka berasal dari Indonesia
+ */
---
+++ jawaban2-6.js
@@ -0,0 +1,34 @@
+/* Soal 2A
+ * Soal: Kenapa baris 21, 22, dan 23 tidak dapat tampil?
+ * Jawaban: Baris 21,22,23 tidak dapat tampil dikarenakan value variable terdaftar adalah false.
+ */
+
+/* Soal 2B
+ * Soal: Kenapa deklarasi pada baris 26 menyebabkan Error?
+ * Jawaban: Deklarasi pada baris 26 menyebabkan Error karena const nama tidak dapat diubah.
+ */
+
+/* Soal 2C
+ * Soal: Dengan melakukan komen pada baris 26, apakah baris 28 dapat dieksekusi?
+ * Jawaban: Tidak, karena variable asal di baris 26, di deklarasikan di dalam scope function perkenalan.
+ */
+
+// Soal 3 - Lakukan destructuring array berikut, sehingga Budi tersimpan pada variable a, Sita tersimpan pada variable b, dan Ayu tersimpan pada variable c.
+const foo = ['Budi', 'Sita', 'Ayu'];
+let [a, b, c] = foo;
+console.log('Hasil nomor 3: ', a, b, c); // Budi Sita Ayu
+
+// Soal 4 - Ubah tanda (-) menjadi (/), sehingga output menjadi ["10/17","05/19","20/19"]
+let bdays = ['10-17', '05-19', '20-19'];
+bdays = bdays.map((bday) => bday.replace('-', '/'));
+console.log('Hasil nomor 4: ', bdays); // ["10/17","05/19","20/19"]
+
+// Soal 5 - Kalikan setiap value dalam array dengan 2, sehingga output menjadi [2, 4, 6, 8, 10, 12]
+let value = [1, 2, 3, 4, 5, 6];
+value = value.map((val) => (val *= 2));
+console.log('Hasil nomor 5: ', value); // [2, 4, 6, 8, 10, 12]
+
+// Soal 6 - Bulatkan setiap value dalam array, sehingga output menjadi [2, 3, 5, 12]
+let arr = [1.5, 2.56, 5.1, 12.33];
+arr = arr.map((val) => Math.round(val));
+console.log('Hasil nomor 6: ', arr); // [2, 3, 5, 12]
